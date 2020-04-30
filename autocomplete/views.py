45a04e1ec370e2e49065384ac630e6dcfb35330a
jerem33620 <jeremyguy@orange.fr>
2020-04-30 15:21:54 +0200

    Modification du fichier views.py

--- autocomplete/views.py
+++ autocomplete/views.py
@@ -5,7 +5,8 @@ from products.models import Product
 
 
 def complete(request):
-    searched_name_product = request.GET.get("searched_name")
-    product = Product.objects.find_products(searched_name_product)
-    product = [product.product_name for product in products]
-    return JsonResponse(product, safe=False)
+    searched_name_product = request.GET.get("term")
+    products = Product.objects.filter(product_name__icontains=searched_name_product).order_by("-nutrition_grade_fr")[:20]
+    products = [product.product_name for product in products]
+
+    return JsonResponse(products, safe=False)
