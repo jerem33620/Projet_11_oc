c7b6e4a8d39644cc3c0e31c9c11a27a6fe626855
jerem33620 <jeremyguy@orange.fr>
2020-05-05 16:13:43 +0200

    Ajout de chromedriver.exe, modif du fichier .coverage et du fichier test_fonctionnal.py

--- tests/test_fonctionnal.py
+++ tests/test_fonctionnal.py
@@ -1,12 +1,36 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.contrib.auth import get_user_model
 
 from selenium import webdriver
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.headless = True
 
-browser = webdriver.Chrome(chrome_options=options)
+class SeleniumChromeFunctionalTestCases(StaticLiveServerTestCase):
+    """Tests fonctionnels utilisant le navigateur Web GoogleChrome."""
 
-class SeleniumTest(StaticLiveServerTestCase):
-    pass
+    def setUp(self):
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+        self.driver.get(self.live_server_url)
+
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
+
+        User = get_user_model()
+        User.objects.create_user(username="jerem", password="Jerem33")
+
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_user_can_disconnect_and_connect(self):
+        self.driver.find_element_by_css_selector('#button-login').click()
+        self.driver.find_element_by_css_selector('#id_username').send_keys("jerem")
+        self.driver.find_element_by_css_selector('#id_password').send_keys("Jerem33")
+        self.driver.find_element_by_css_selector('#button-submit').click()
+
+        logout = self.driver.find_element_by_css_selector('#button-logout')
+
+        self.assertEqual(logout.text, "", "Le bouton de déconnexion devrait être disponible.")
+    
